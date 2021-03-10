99dbc04450071de694323c0a1b90a9458f03de7f
Eric Jankowski <ericjankowski@boisestate.edu>
2021-03-10 14:52:16 -0700

    Noticed that there's 80 points in 0.raw

--- plot-scratch.py
+++ plot-scratch.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
-a = numpy.loadtxt('samples/1.raw',skiprows=1,delimiter=',')
+a = numpy.loadtxt('samples/0.raw',skiprows=1,delimiter=',')
 plt.plot(a[:,0],a[:,1])
 plt.show()
+print(len(a))
