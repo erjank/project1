ee78d0e258adb298ab9caae45c6f55588a7cc090
Eric Jankowski <ericjankowski@boisestate.edu>
2021-03-10 14:54:22 -0700

    Made some plans for what to code next in plot-scratch.py

--- plot-scratch.py
+++ plot-scratch.py
@@ -4,3 +4,13 @@ a = numpy.loadtxt('samples/0.raw',skiprows=1,delimiter=',')
 plt.plot(a[:,0],a[:,1])
 plt.show()
 print(len(a))
+
+#I want to get the slopes of the initial linear parts (that's the Young's modulus)
+	#Find initial linear part (these have different numbers of points in them!)
+	#We can use stuff from notebook4? 3? to get the linear regression of that inital part
+
+
+#I want to average that over all of the samples
+
+
+
